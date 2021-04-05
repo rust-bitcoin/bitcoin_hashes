ad3a777fd30ef4bd2df5555b97518869283062a8
Jeremy Rubin <j@rubin.io>
2021-04-05 13:19:20 -0700

    Revert "remove schemars"
    This reverts commit 4665e8ddb11cdc75f2cb05432e7a7b4fdf29bafd.

--- src/util.rs
+++ src/util.rs
@@ -299,6 +299,29 @@ macro_rules! hash_newtype {
     };
 }
 
+#[cfg(feature = "schemars")]
+pub mod json_hex_string {
+    use schemars::schema::{Schema, SchemaObject};
+    use schemars::{gen::SchemaGenerator, JsonSchema};
+    macro_rules! define_custom_hex {
+        ($name:ident, $len:expr) => {
+            pub fn $name(gen: &mut SchemaGenerator) -> Schema {
+                let mut schema: SchemaObject = <String>::json_schema(gen).into();
+                schema.string = Some(Box::new(schemars::schema::StringValidation {
+                    max_length: Some($len * 2),
+                    min_length: Some($len * 2),
+                    pattern: Some("[0-9a-fA-F]+".to_owned()),
+                }));
+                schema.into()
+            }
+        };
+    }
+    define_custom_hex!(len_8, 8);
+    define_custom_hex!(len_20, 20);
+    define_custom_hex!(len_32, 32);
+    define_custom_hex!(len_64, 64);
+}
+
 #[cfg(test)]
 mod test {
     use Hash;
