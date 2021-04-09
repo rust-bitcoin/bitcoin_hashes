be392299bccfe239111f0c50444af50ab0ae606e
Riccardo Casatta <riccardo@casatta.it>
2021-04-09 15:27:36 +0200

    add no_std program and build it in github actions

---
+++ embedded/src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate bitcoin_hashes;
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+use bitcoin_hashes::sha256;
+use core::str::FromStr;
+use bitcoin_hashes::Hash;
+
+hash_newtype!(TestType, sha256::Hash, 32, doc="test");
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Hello world!").unwrap();
+
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {
+        // your code goes here
+    }
+}
