aece2e884dc3037e3862688be2dd535dc36290be
Riccardo Casatta <riccardo@casatta.it>
2021-04-13 12:12:29 +0200

    remove config files

--- embedded/src/main.rs
+++ embedded/src/main.rs
@@ -4,12 +4,7 @@
 #[macro_use]
 extern crate bitcoin_hashes;
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-
+use panic_halt as _;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
 use bitcoin_hashes::sha256;
@@ -23,7 +18,5 @@ fn main() -> ! {
 
     debug::exit(debug::EXIT_SUCCESS);
 
-    loop {
-        // your code goes here
-    }
+    loop {}
 }
