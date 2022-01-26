f6603c2b456aed08dbcceacd01424d7e6187bfae
Tobin Harding <me@tobin.cc>
2022-01-27 10:18:00 +1100

    Run tests without std enabled
    Currently when we run the test suite we always enable the "std" feature.
    There are tests that can be run without "std", we should run these
    without "std" to better test our no-std code.

--- contrib/test.sh
+++ contrib/test.sh
@@ -28,16 +28,16 @@ if [ "$DO_FEATURE_MATRIX" = true ]; then
 
     # All features
     cargo build --all --no-default-features --features="$FEATURES"
-    cargo test --all --features="$FEATURES"
+    cargo test --all --no-default-features --features="$FEATURES"
     # Single features
     for feature in ${FEATURES}
     do
         cargo build --all --no-default-features --features="$feature"
-        cargo test --all --features="$feature"
+        cargo test --all --no-default-features --features="$feature"
 		# All combos of two features
 		for featuretwo in ${FEATURES}; do
 			cargo build --all --no-default-features --features="$feature $featuretwo"
-			cargo test --all --features="$feature $featuretwo"
+			cargo test --all --no-default-features --features="$feature $featuretwo"
 		done
     done
 
@@ -67,11 +67,11 @@ if [ "$DO_ASAN" = true ]; then
     CC='clang -fsanitize=address -fno-omit-frame-pointer'                                        \
     RUSTFLAGS='-Zsanitizer=address -Clinker=clang -Cforce-frame-pointers=yes'                    \
     ASAN_OPTIONS='detect_leaks=1 detect_invalid_pointer_pairs=1 detect_stack_use_after_return=1' \
-    cargo test --lib --all --features="$FEATURES" -Zbuild-std --target x86_64-unknown-linux-gnu
+    cargo test --lib --all --no-default-features --features="$FEATURES" -Zbuild-std --target x86_64-unknown-linux-gnu
     cargo clean
     CC='clang -fsanitize=memory -fno-omit-frame-pointer'                                         \
     RUSTFLAGS='-Zsanitizer=memory -Zsanitizer-memory-track-origins -Cforce-frame-pointers=yes'   \
-    cargo test --lib --all --features="$FEATURES" -Zbuild-std --target x86_64-unknown-linux-gnu
+    cargo test --lib --all --no-default-features --features="$FEATURES" -Zbuild-std --target x86_64-unknown-linux-gnu
 fi
 
 # Bench
